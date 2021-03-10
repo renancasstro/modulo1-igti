d9eb7739f4e5e7184ea73be07ecc2b8fcdf1543c
Renan Castro <renancruz.castro@gmail.com>
2021-03-10 00:39:17 -0300

    commit

---
+++ trabalho_pratico_1.py
@@ -0,0 +1,22 @@
+"""
+Os alunos deverão desempenhar as seguintes atividades:
+1. Crie a variável número 1: variavel_1 = ('Uva', 'Abacate', 'Laranja', 'Melancia')
+
+2. Crie a variável número 2: variavel_2 = [2.1,25.2,35.1,85.5,21.5]
+
+3. Crie a variável número 3: variavel_3 = {'MG':31, 'SP':35, 'RJ':33, 'ES':32}
+"""
+
+variavel_1 = ('Uva', 'Abacate', 'Laranja', 'Melancia')
+
+variavel_2 = [2.1,25.2,35.1,85.5,21.5]
+
+variavel_3 = {'MG':31, 'SP':35, 'RJ':33, 'ES':32}
+
+#Uso do comando type() para descoberta do tipo da variável:
+
+print(type(variavel_1))
+
+print(type(variavel_2))
+
+print(type(variavel_3))
