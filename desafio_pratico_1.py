81941e130a31dfe74a2e13de9c67671c216e8f17
Renan Castro <renancruz.castro@gmail.com>
2021-03-16 17:10:20 -0300

    commit desafio

---
+++ desafio_pratico_1.py
@@ -0,0 +1,42 @@
+"""
+Os alunos deverão desempenhar as seguintes atividades: 
+1. Crie a função número 1: 
+
+soma = lambda x, y: x + y
+
+2. Crie a função número 2:  
+
+def soma(x, y): 
+    print (x + y)
+
+3. Crie o seguinte laço de repetição:
+
+num = 0 
+while num < 11: 
+    print(num) 
+    num += 1
+
+4. Importe o pacote pandas: 
+
+import pandas as pd
+
+5. Execute o seguinte código:
+
+for i in range(1,101): 
+    print (i)
+"""
+
+soma = lambda x, y: x + y
+
+def soma(x, y): 
+    print (x + y)
+
+num = 0 
+while num < 11: 
+    print(num) 
+    num += 1
+
+import pandas as pd
+
+for i in range(1,101): 
+    print (i)
